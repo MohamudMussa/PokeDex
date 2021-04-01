39de3305c961994250f60e50c5f5d8831c79b9e0
Mohamud Mussa <70893158+MohamudMussa@users.noreply.github.com>
2021-04-01 13:35:42 +0100

    Setting up Pokedex

---
+++ pokedex/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+function App() {
+  return (
+    <div class="app-wrapper">
+
+      <h1> Hello World </h1>
+
+    </div>
+
+
+
+  )
+
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
